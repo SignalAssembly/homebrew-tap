c249866bfb6f908a51b216ba21d5e7ad7ee60f24
publish-release <release@signalassembly.com>
2026-07-25 10:05:50 -0500

    actual-files 0.77.13

--- Casks/actual-files.rb
+++ Casks/actual-files.rb
@@ -1,17 +1,17 @@
 cask "actual-files" do
-  version "0.76.58"
+  version "0.77.13"
 
   on_arm do
-    sha256 "3b1a835d4af55c4ed3d4068c5e4bc3d3fab72b021ff5a648434fde47c4bb8526"
+    sha256 "80d0d1018fb1a5bdffe8fd7b2bdc3b9e0cee0b8565dd5a9c117c69ad76d2fdb6"
     url "https://signalassembly.com/dl/actual-files-v#{version}-Actual.Files_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "443dcc2dd2e46267d06abefa2cc9281ad56e07737c9f8b0c7498c843f6c2fa6b"
+    sha256 "559381c0cb5c8b126318418bcbe5801b6dd182036ff3fa4aaa6f2701e59ae8d3"
     url "https://signalassembly.com/dl/actual-files-v#{version}-Actual.Files_#{version}_x64.dmg"
   end
 
   name "Actual Files"
-  desc "Dual-pane file manager in the Directory Opus tradition"
+  desc "Cross-platform file manager for power users"
   homepage "https://signalassembly.com/products/actual-files"
 
   app "Actual Files.app"
