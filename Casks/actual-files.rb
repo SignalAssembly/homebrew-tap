aafa2aa3bf8f43f77bc2a0c86639e3d8d3a5785e
publish-release <release@signalassembly.com>
2026-08-06 16:02:33 -0500

    actual-files 0.77.93

--- Casks/actual-files.rb
+++ Casks/actual-files.rb
@@ -1,12 +1,12 @@
 cask "actual-files" do
-  version "0.77.92"
+  version "0.77.93"
 
   on_arm do
-    sha256 "14a30806174d271de746a6c2bf5e502b3ef7c762bb0459d5d2667e0f7765d99d"
+    sha256 "99a66ad4180adae4b1e71cc5b799c3aac85a182b198a56eace9c43f1e7c109b5"
     url "https://downloads.signalassembly.com/actual-files-v#{version}-Actual.Files_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "9201394d994d38e270ac671f21bf87eef9c1c40b9a76feebdfac205fb6521b8a"
+    sha256 "85eb6841e8e72abfb2437eb815a22ceca6e4576c41739e15c9324a1c7707ad71"
     url "https://downloads.signalassembly.com/actual-files-v#{version}-Actual.Files_#{version}_x64.dmg"
   end
 
